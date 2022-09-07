f366f7d93bb59e8d0aa0218595e72cc2a6233532
Saoa35 <saoamah@gmail.com>
2022-09-07 20:32:36 +0200

    + handleChange & state

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,13 +2,31 @@ import React from 'react'
 
 class Form extends React.Component {
 
+    state = {
+        value: '',
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+
+        const value = e.target.value
+        this.setState({
+            value
+        })
+    }
+
     render() {
 
         return(
 
             <div>
                 <form>
-                
+                    <label>First Name</label>
+                    <input type='text' value={this.state.value} onChange={this.handleChange}></input>
                 </form>
             </div>
             
