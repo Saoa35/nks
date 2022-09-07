658925cd53211daf8e27a5b7698652b62eec5d1a
Saoa35 <saoamah@gmail.com>
2022-09-07 21:32:02 +0200

    added className='submitButton'

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -9,6 +9,7 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
@@ -19,14 +20,36 @@ class Form extends React.Component {
         })
     }
 
+    handleSubmit(e) {
+
+        e.preventDefault();
+        console.log(this.state.value);
+    }
+    
+
     render() {
 
         return(
 
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
+                    <p>
                     <label>First Name</label>
                     <input type='text' value={this.state.value} onChange={this.handleChange}></input>
+                    </p>
+                    <p>
+                    <label>Last Name</label>
+                    <input type='text' value={this.state.value} onChange={this.handleChange}></input>
+                    </p>
+                    <p>
+                    <label>Date of birth</label>
+                    <input type='date' value={this.state.value} onChange={this.handleChange}></input>
+                    </p>
+                    <p className='submitButton'>
+                        <button>
+                            Submit
+                        </button>
+                    </p>
                 </form>
             </div>
             
