6d5f5d254105abcaff8f3ed82ef2d58f40d59447
Saoa35 <saoamah@gmail.com>
2022-09-07 18:24:08 +0200

    created Form component & putted it in App

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Form extends React.Component {
+
+    render() {
+
+        return(
+
+            <div>
+                <form>
+                
+                </form>
+            </div>
+            
+        )
+    }
+}
+
+export default Form;
