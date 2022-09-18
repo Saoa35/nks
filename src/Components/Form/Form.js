93443986ad2110276227a21907ae476333a84e96
Saoa35 <saoamah@gmail.com>
2022-09-18 16:54:02 +0300

    created UserRoles component

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -50,7 +50,8 @@ class Form extends React.Component {
         <div className='Form'>
           <div className='static_header'>
             <div className='user_information'>User Information</div>
-            <div className='user_roles'>User Roles</div>
+            <div style={{width:'15%'}}><hr/></div>
+            <div className='user_roles'>Applications, Systems & User Roles</div>
           </div>
           <div className='create-user-header'>
             <p className='foto'><span>Bild</span></p>
