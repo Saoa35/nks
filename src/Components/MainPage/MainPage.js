24598abaee2af82f08fdcaabe192429b7ac4257f
Saoa35 <saoamah@gmail.com>
2023-02-12 14:36:41 +0100

    changed to func component Main Page

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import './MainPage.css';
-import Form from '../Form/Form';
+import React from "react";
+import "./MainPage.css";
+import Form from "../Form/Form";
 
+export default function MainPage() {
+  return (
+    <div className="MainPage">
+      <header className="main_header">
+        <div className="user_management">
+          <span>Usermanagement</span>
+        </div>
+        <div className="user_initials">
+          <p>JH</p>
+        </div>
+      </header>
 
-export default class MainPage extends React.Component {
+      <main className="main_main">
+        <aside className="main_aside">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li style={{ height: "85%", border: "1px solid #E8E8E8" }}></li>
+          <li
+            className="main_aside_last_li"
+            style={{ border: "1px solid #E8E8E8" }}
+          >
+            <p></p>
+          </li>
+        </aside>
 
-  render() {
-
-    return(
-      <div className='MainPage'>
-        <header className='main_header'>
-          <div className='user_management'>
-            <span>Usermanagement</span>
+        <section className="main_section">
+          <div className="page_classification">
+            <button className={"page_classification_button"}></button>
+            <p style={{ color: "#333333", fontSize: "3vh" }}>Create User</p>
           </div>
-          <div className='user_initials'>
-            <p>JH</p>
+          <div className="main_field">
+            <Form />
           </div>
-        </header>
-
-        <main className='main_main'>
-          <aside className='main_aside'>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li style={{height:'85%', border: '1px solid #E8E8E8'}}></li>
-              <li className='main_aside_last_li' style={{border: '1px solid #E8E8E8'}}><p></p></li>
-          </aside>
-          
-          <section className='main_section'>
-          <div className='page_classification'>
-              <button className={'page_classification_button'}></button>
-              <p style={{color:'#333333', fontSize:'3vh'}}>Create User</p>
-            </div>
-          <div className='main_field'>
-             <Form/>
-          </div>
-          </section>
-        </main>
-      </div>
-    )
-  }
+        </section>
+      </main>
+    </div>
+  );
 }
