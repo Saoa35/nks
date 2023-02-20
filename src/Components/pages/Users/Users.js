a1a8e183bb4f51f746937be001c260fb1b25f94a
Saoa35 <saoamah@gmail.com>
2023-02-20 18:05:26 +0100

    stylized user-text class

--- src/Components/pages/Users/Users.js
+++ src/Components/pages/Users/Users.js
@@ -4,7 +4,7 @@ export const Users = () => {
   return (
     <div className="users-main_div">
       <p className="user-login-image"></p>
-      <p>Add a new user</p>
+      <p className="user-text">Add a new user</p>
     </div>
   );
 };
