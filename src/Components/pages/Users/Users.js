1c5d757798b4e49cd87e6dc7401d7a0ed3c93d50
Saoa35 <saoamah@gmail.com>
2023-02-22 15:25:32 +0100

    created button_wrapper class

--- src/Components/pages/Users/Users.js
+++ src/Components/pages/Users/Users.js
@@ -5,7 +5,9 @@ export const Users = () => {
     <div className="users-main_div">
       <p className="user-login-image"></p>
       <p className="user-text">Add a new user</p>
-      <button className="add-user_button">Create User</button>
+      <p className="button_wrapper">
+        <button className="add-user_button">Create User</button>
+      </p>
     </div>
   );
 };
