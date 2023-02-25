f9855a880de9b2194b01b6b79726a4f50b33d903
Saoa35 <saoamah@gmail.com>
2023-02-25 09:27:05 +0100

    + page_classification div to Users comp.

--- src/Components/pages/Users/Users.js
+++ src/Components/pages/Users/Users.js
@@ -3,14 +3,22 @@ import "./Users.css";
 
 export const Users = () => {
   return (
-    <div className="users-main_div">
-      <p className="user-login-image"></p>
-      <p className="user-text">Add a new user</p>
-      <p className="button_wrapper">
-        <Link to="/forms">
-          <button className="add-user_button">Create User</button>
-        </Link>
-      </p>
-    </div>
+    <>
+      <div className="page_classification">
+        {/* <Link to="/">
+          <button className="page_classification_button"></button>
+        </Link> */}
+        <p style={{ color: "#333333", fontSize: "3vh" }}>Users</p>
+      </div>
+      <div className="users-main_div">
+        <p className="user-login-image"></p>
+        <p className="user-text">Add a new user</p>
+        <p className="button_wrapper">
+          <Link to="/forms">
+            <button className="add-user_button">Create User</button>
+          </Link>
+        </p>
+      </div>
+    </>
   );
 };
