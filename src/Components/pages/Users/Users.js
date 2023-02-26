c3b785968009998362d324821e9f66495216b62a
Saoa35 <saoamah@gmail.com>
2023-02-26 11:02:41 +0100

    + add_button

--- src/Components/pages/Users/Users.js
+++ src/Components/pages/Users/Users.js
@@ -9,6 +9,7 @@ export const Users = () => {
           <button className="page_classification_button"></button>
         </Link> */}
         <p style={{ color: "#333333", fontSize: "3vh" }}>Users</p>
+        <button className="add_button">+</button>
       </div>
       <div className="users-main_div">
         <div className="logoImage_wrapper">
