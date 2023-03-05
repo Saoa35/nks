92c7d0741cc917c7c89c0aae128bbc192dd9d251
Saoa35 <saoamah@gmail.com>
2023-03-05 10:12:53 +0100

    created UsRoles comp.

---
+++ src/Components/pages/UsRoles/UsRoles.js
@@ -0,0 +1,11 @@
+const UsRoles = () => {
+  return (
+    <div className="users_wrapper">
+      <div className="page_classification">
+        <p style={{ color: "#333333", fontSize: "3vh" }}>User Roles</p>
+      </div>
+    </div>
+  );
+};
+
+export default UsRoles;
