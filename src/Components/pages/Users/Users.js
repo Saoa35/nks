14849faa414150767f6ce62d44992d3a15261db3
Saoa35 <saoamah@gmail.com>
2023-03-18 08:05:57 +0100

    added svg`s on buttons

--- src/Components/pages/Users/Users.js
+++ src/Components/pages/Users/Users.js
@@ -16,6 +16,12 @@ export const Users = () => {
       .catch((error) => console.error(error));
   }, []);
 
+  function firstLetters(str) {
+    const toArr = str.split(" ");
+    const firstLet = toArr.map((el) => (el = el[0]));
+    return firstLet.join("");
+  }
+
   return (
     <div className="Users">
       <div className="page_classification justify-between">
@@ -34,13 +40,24 @@ export const Users = () => {
               placeholder="Search"
             />
             <div>
-              <p></p>
+              <div>
+                <p></p>
+              </div>
             </div>
           </div>
           <ul>
             {users?.map((el, i) => (
               <li key={i}>
-                <div className="first_letters"></div>
+                <div className="first_letters">
+                  <p>{firstLetters(el.name)}</p>
+                </div>
+                <div
+                  className={
+                    el.address.zipcode.startsWith("5")
+                      ? "ofline_status"
+                      : "online_status"
+                  }
+                ></div>
                 <div className="users_names">
                   <p>{el.name}</p>
                   <p>{el.company.name}</p>
@@ -54,11 +71,61 @@ export const Users = () => {
             <p className="user-login-image"></p>
           </div>
           <p className="user-text">Add a new user</p>
-          <p className="button_wrapper">
+          <div className="button_wrapper">
             <Link to="/forms">
-              <button className="add-user_button ">Create User</button>
+              <div className="add-user_button ">
+                <p>
+                  <svg
+                    width="33"
+                    height="20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g>
+                      <title>Layer 1</title>
+                      <ellipse
+                        ry="5"
+                        rx="5"
+                        id="svg_1"
+                        cy="5.72494"
+                        cx="12.16951"
+                        stroke="#000"
+                        fill="#fff"
+                      />
+                      <ellipse
+                        stroke="#000"
+                        ry="4.23774"
+                        rx="11.70042"
+                        id="svg_2"
+                        cy="14.94669"
+                        cx="12.30277"
+                        fill="#fff"
+                      />
+                      <line
+                        id="svg_3"
+                        y2="8.20362"
+                        x2="31.86033"
+                        y1="8.15032"
+                        x1="21.78571"
+                        stroke="#000"
+                        fill="none"
+                      />
+                      <line
+                        transform="rotate(0.709444 26.6631 7.9371)"
+                        stroke="#000"
+                        id="svg_4"
+                        y2="13.00106"
+                        x2="26.68976"
+                        y1="2.87313"
+                        x1="26.63645"
+                        fill="none"
+                      />
+                    </g>
+                  </svg>
+                </p>
+                <p>Create User</p>
+              </div>
             </Link>
-          </p>
+          </div>
         </div>
       </div>
     </div>
