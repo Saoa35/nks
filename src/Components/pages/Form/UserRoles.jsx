da32c87cdce4b6c8b34335d05f98e551f33d4abb
Saoa35 <saoamah@gmail.com>
2023-04-08 15:51:58 +0200

    changed Applications, Systems and User Roles styles

--- src/Components/pages/Form/UserRoles.jsx
+++ src/Components/pages/Form/UserRoles.jsx
@@ -30,12 +30,69 @@ export const UserRoles = memo(() => {
         <ul>
           {users.map((el, i) => (
             <li key={i}>
-              <input type="checkbox" />
-              <label>{el.address.suite}</label>
+              <input type="checkbox" id={el.address.suite} />
+              <label htmlFor={el.address.suite}>{el.address.suite}</label>
               <div>
-                <input type="checkbox" />
-                <p></p>
-                <label>{el.address.street}</label>
+                <input type="checkbox" id={el.address.street} />
+                <label htmlFor={el.address.street}>
+                  <p>
+                    <svg
+                      width="25"
+                      height="25"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g>
+                        <title>Aplications & Systems</title>
+                        <rect
+                          id="svg_1"
+                          height="5"
+                          width="5"
+                          y="4.94903"
+                          x="1.31629"
+                          stroke="#2fa6b5"
+                          fill="#bfe4e8"
+                        />
+                        <rect
+                          id="svg_7"
+                          height="5"
+                          width="5"
+                          y="4.94903"
+                          x="18.2835"
+                          stroke="#2fa6b5"
+                          fill="#bfe4e8"
+                        />
+                        <rect
+                          id="svg_8"
+                          height="5"
+                          width="5"
+                          y="15.44083"
+                          x="10.74252"
+                          stroke="#2fa6b5"
+                          fill="#bfe4e8"
+                        />
+                        <line
+                          id="svg_9"
+                          y2="7.33606"
+                          x2="15.68033"
+                          y1="7.41803"
+                          x1="6.41803"
+                          stroke="#2fa6b5"
+                          fill="none"
+                        />
+                        <line
+                          id="svg_10"
+                          y2="13.97541"
+                          x2="9.61475"
+                          y1="10.28688"
+                          x1="6.58197"
+                          stroke="#2fa6b5"
+                          fill="none"
+                        />
+                      </g>
+                    </svg>
+                  </p>
+                  <p>{el.address.street}</p>
+                </label>
               </div>
             </li>
           ))}
