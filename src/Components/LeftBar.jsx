61875f2bdb48dd1c9f49978ec93e759ca7ad5bcf
Saoa35 <saoamah@gmail.com>
2023-05-11 11:10:32 +0400

    added rerender check

--- src/Components/LeftBar.jsx
+++ src/Components/LeftBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import styles from "./LeftBar.module.scss";
 
 export const LeftBar = memo(({ changeStateToFalse }) => {
-  // console.log("LeftBar rerander");
+  console.log("LeftBar rerander");
 
   return (
     <aside>
